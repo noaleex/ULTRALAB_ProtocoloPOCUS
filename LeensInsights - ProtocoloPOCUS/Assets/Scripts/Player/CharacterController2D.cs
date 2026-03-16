3def4d3e26f08daacc2cb12ee0184fd747979f9f
Guigz1415 <bngpacheco9@gmail.com>
2026-03-16 17:55:55 -0300

    Move
    Tentando aplicar movimentação

--- LeensInsights - ProtocoloPOCUS/Assets/Scripts/Player/CharacterController2D.cs
+++ LeensInsights - ProtocoloPOCUS/Assets/Scripts/Player/CharacterController2D.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class CharacterController2D : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody2D rigidbody2d;
+    [SerializeField] float speed = 2f;
+    Vector2 motionVector;
+    
+    void Awake()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        motionVector = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+            );
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rigidbody2d.velocity = motionVector * speed;
     }
 }
