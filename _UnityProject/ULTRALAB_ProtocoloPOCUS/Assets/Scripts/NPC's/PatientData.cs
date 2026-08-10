5e7b913fb12bf92297b63c94513e9a10e79e3636
Guigz1415 <bngpacheco9@gmail.com>
2026-08-09 21:58:57 -0300

    Conduta1.0

--- _UnityProject/ULTRALAB_ProtocoloPOCUS/Assets/Scripts/NPC's/PatientData.cs
+++ _UnityProject/ULTRALAB_ProtocoloPOCUS/Assets/Scripts/NPC's/PatientData.cs
@@ -15,10 +15,43 @@ public class PatientData : ScriptableObject
 
     [Header("Caso")]
     [TextArea]
-    public string caso;
+    public string resumeCaso;
 
     [TextArea]
     public string physicalExam;
+
+    [Header("Via Aérea")]
+    public string permeabilidade;
+    public string presenca;
+    public bool intervenção;
+
+    [Header("Respiração")]
+    public string frequencia;
+    public string saturation;
+    public bool acessoria;
+    public string padraoRespiratorio;
+    public string asculta;
+    public string expansibilidade;
+    public string oxigenoterapiaTipo;
+    public string oxigenoterapiaFluxo;
+
+    [Header("Circulação")]
+    public string frequenciaCardiaca;
+    public string pressaoArterial;
+    public string perfusaoTempo;
+    public string perfusaoExtremidades;
+    public string pulsos;
+    public string ritmoCardiaco;
+    public string edema;
+    public string temperatura;
+
+    [Header("Avaliação neurológica")]
+    public string nivelConsciencia;
+
+    [Header("Exposição")]
+    public string avalicaoPele;
+    public string presencaPele;
+    public string dorEscala;
 }
 
 public static class CurrentPatient
